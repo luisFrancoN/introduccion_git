3bf2bb66172284c4565cd3dfb12a04ad3451fee6
Luis Franco <j.luisfranco.cucei@gmail.com>
2020-09-21 18:22:30 -0500

    se capturo el nombre y edad del usuario

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main()  {
+	var nombre string
+	var edad int
 	fmt.Println("calando Git")
+	fmt.Printf("cual es tu nombre: ")
+	fmt.Scanf("%s\n",&nombre)
+	fmt.Printf("cual es tu edad: ")
+	fmt.Scanf("%d\n",&edad)
+
+	fmt.Println("Que onda",nombre,"\ntu edad es",edad)
 }
